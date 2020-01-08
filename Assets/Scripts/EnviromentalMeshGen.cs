cf7e7d4b430494afaf7aa4894dff4f0d61ec13e2
rubyleehs <rubyleehs@live.com>
2020-01-08 19:22:53 +0000

    Enviromental Mesh Gen

---
+++ Assets/Scripts/EnviromentalMeshGen.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnviromentalMeshGen : GridMeshGen
+{
+    [SerializeField] private Vector2Int mapResolutionInRegions = new Vector2Int(10, 10);
+    [SerializeField] private int cellsPerMapRegion = 5;
+    private Vector2Int mapResolution;
+
+    [SerializeField] private Vector2 globalHeightMinMax = new Vector2(0, 150);
+    [SerializeField] private Vector2 regionHeightMinMax = new Vector2(0, 2);
+
+    [SerializeField] private int heightMapAverageMaskRange = 3; //Decreases height variance in the micro scale.
+
+    [SerializeField] private int regionMapAverageMaskRange = 1; //Decreases height variance in the macro scale. 
+
+    private float[,] globalHeightMap; //height map are for the corners of a cell, its a 2d array so it is easeir to implement more stuff in future. coverted to 1d later on
+    private float[,] regionHeightMap;
+
+
+    private void Awake()
+    {
+        Generate();
+    }
+
+    private void Generate()
+    {
+        mapResolution = mapResolutionInRegions * cellsPerMapRegion;
+        regionHeightMap = IncreaseResolutionOf2DArray(AverageNearby(CreateRandom2DArray(mapResolutionInRegions, regionHeightMinMax.x, regionHeightMinMax.y), regionMapAverageMaskRange), cellsPerMapRegion);
+        globalHeightMap = CreateRandom2DArray(mapResolution, globalHeightMinMax.x, globalHeightMinMax.y);
+        for (int y = 0; y < mapResolution.y; y++)
+        {
+            for (int x = 0; x < mapResolution.x; x++)
+            {
+                globalHeightMap[x, y] *= regionHeightMap[x, y];
+            }
+        }
+        globalHeightMap = AverageNearby(globalHeightMap, heightMapAverageMaskRange);
+        globalHeightMap = AverageNearby(globalHeightMap, 1); //Average once more to smooth things out/prevent large diffrences.
+        GenerateMesh(globalHeightMap, new Vector3(1, 0.1f, 1), Vector3.zero);
+    }
+
+    private float[,] IncreaseResolutionOf2DArray(float[,] arr, int multiplier)
+    {
+        float[,] result = new float[arr.GetLength(0) * multiplier, arr.GetLength(1) * multiplier];
+        float val;
+        for (int y = 0; y < arr.GetLength(1); y++)
+        {
+            for (int x = 0; x < arr.GetLength(0); x++)
+            {
+                val = arr[x, y];
+                for (int dy = 0; dy < multiplier; dy++)
+                {
+                    for (int dx = 0; dx < multiplier; dx++)
+                    {
+                        result[x * multiplier + dx, y * multiplier + dy] = val;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    private float[,] CreateRandom2DArray(Vector2Int size, float min, float max)
+    {
+        float[,] result = new float[size.x, size.y];
+        for (int y = 0; y < size.y; y++)
+        {
+            for (int x = 0; x < size.x; x++)
+            {
+                result[x, y] = Random.Range(min, max);
+            }
+        }
+        return result;
+    }
+
+    private float[,] AverageNearby(float[,] arr, int range)
+    {
+        float[,] result = new float[arr.GetLength(0), arr.GetLength(1)]; //
+        int count;
+        int cx, cy;
+        for (int y = 0; y < arr.GetLength(1); y++)
+        {
+            for (int x = 0; x < arr.GetLength(0); x++)
+            {
+                count = 0;
+                for (int dy = -range; dy <= range; dy++)
+                {
+                    for (int dx = -range; dx <= range; dx++)
+                    {
+                        cx = x + dx;
+                        cy = y + dy;
+
+                        if (cx < 0 || cy < 0 || cx >= arr.GetLength(0) || cy >= arr.GetLength(1)) continue;
+
+                        result[x, y] += arr[cx, cy];
+                        count++;
+                    }
+                }
+                result[x, y] /= count;
+            }
+        }
+        return result;
+    }
+
+
+    private T[] FlattenTo1DArray<T>(ref T[,] arr)
+    {
+        T[] result = new T[arr.GetLength(0) * arr.GetLength(1)];
+
+        for (int y = 0, i = 0; y < arr.GetLength(1); y++)
+        {
+            for (int x = 0; x < arr.GetLength(0); x++, i++)
+            {
+                result[i] = arr[x, y];
+            }
+        }
+        return result;
+    }
+}
---
+++ Assets/Scripts/MeshGen.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class GridMeshGen : MonoBehaviour
+{
+    protected Vector2Int meshResolution;
+    protected Vector3 cellSize;
+
+    private Vector3[] meshVertices;
+    private Mesh mesh;
+
+    protected void GenerateMesh(float[,] meshVerticesHeight, Vector3 cellSize, Vector3 bottomRightCorner)
+    {
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "Procedural Grid";
+
+        this.meshResolution = new Vector2Int(meshVerticesHeight.GetLength(0), meshVerticesHeight.GetLength(1));
+        this.cellSize = cellSize;
+        meshVertices = new Vector3[(meshResolution.x) * (meshResolution.y)];
+        Vector2[] uv = new Vector2[meshVertices.Length];
+        for (int i = 0, y = 0; y < meshResolution.y; y++)
+        {
+            for (int x = 0; x < meshResolution.x; x++, i++)
+            {
+                meshVertices[i] = bottomRightCorner + new Vector3(x * cellSize.x, 0, y * cellSize.z);
+                uv[i] = new Vector2((float)x / meshResolution.x, (float)y / meshResolution.y); //Create UVs for mesh
+            }
+        }
+        if (meshVerticesHeight != null)
+        {
+            for (int i = 0, y = 0; y < meshResolution.y; y++)
+            {
+                for (int x = 0; x < meshResolution.x; x++, i++)
+                {
+                    meshVertices[i] += Vector3.up * meshVerticesHeight[x, y] * cellSize.y;
+                }
+            }
+        }
+
+        //Create triangles for mesh
+        int[] triangles = new int[(meshResolution.x - 1) * (meshResolution.y - 1) * 6];
+
+        for (int ti = 0, vi = 0, y = 0; y < meshResolution.y - 1; y++, vi++)
+        {
+            for (int x = 0; x < meshResolution.x - 1; x++, ti += 6, vi++)
+            {
+                triangles[ti] = vi;
+                triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+                triangles[ti + 4] = triangles[ti + 1] = vi + meshResolution.x;
+                triangles[ti + 5] = vi + meshResolution.x + 1;
+            }
+        }
+
+        //Debug.Log(meshVertices.Length + " | " + triangles.Length);
+        mesh.vertices = meshVertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (meshVertices == null)
+        {
+            Debug.Log("no vertices found");
+            return;
+        }
+        Gizmos.color = Color.black;
+        for (int i = 0; i < meshVertices.Length; i++)
+        {
+            Gizmos.DrawSphere(meshVertices[i], 0.1f);
+        }
+    }
+}
